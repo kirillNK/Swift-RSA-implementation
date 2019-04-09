8f8e7ade798065050127fb7c684d6cdc694d2059
Kirill <kirill.parhomenko@gmail.com>
2019-04-09 23:23:06 +0500

    ADD: extension for String (add split method)

--- rsa/ViewController.swift
+++ rsa/ViewController.swift
@@ -40,6 +40,7 @@ class ViewController: NSViewController {
     @IBOutlet weak var msgLabel: NSTextField!
     
     @IBAction func encryptBtn(_ sender: NSButton) {
+        //print(arrayOfSumbol)
     }
     @IBAction func decryptBtn(_ sender: NSButton) {
     }
@@ -69,7 +70,7 @@ class ViewController: NSViewController {
     func generateP() -> Int {
         var p: Int = 0
         p = Int.random(in: 1...10000)
-        if (isPrimeFermaMethod(number: p)) == true {
+        if isPrimeFermaMethod(number: p) {
             return p
         } else {
             return 0
@@ -82,7 +83,7 @@ class ViewController: NSViewController {
     func generateQ() -> Int {
         var q: Int = 0
         q = Int.random(in: 1...10000)
-        if (isPrimeFermaMethod(number: q)) == true {
+        if isPrimeFermaMethod(number: q) {
             return q
         } else {
             return 0
@@ -163,7 +164,7 @@ class ViewController: NSViewController {
     func generateD() -> Int { //check func
         var d: Int = 0
         d = Int.random(in: 1...10000)
-        if (isCoPrime(num1: d, num2: euler) == true) {
+        if isCoPrime(num1: d, num2: euler) {
             return d
         } else {
             generateD()
@@ -194,8 +195,29 @@ class ViewController: NSViewController {
     //alphabet arrays
     let symbolArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "э", "ю", "я", "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Э", "Ю", "Я", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "&", "?", "-", "+", "=", "~"]
     
+    let testString = "Super secret message!"
+    
+    lazy var arrayOfSumbol: Array = testString.splitedBy(length: 1)
     
     
+    
+}
+
+extension String {
+    
+    public func splitedBy(length: Int) -> [String] {
+        
+        var result = [String]()
+        
+        for i in stride(from: 0, to: self.count, by: length) {
+            let endIndex = self.index(self.endIndex, offsetBy: -i)
+            let startIndex = self.index(endIndex, offsetBy: -length, limitedBy: self.startIndex) ?? self.startIndex
+            result.append(String(self[startIndex..<endIndex]))
+        }
+        
+        return result.reversed()
+        
+    }
     
 }
 
