97999838b58feb9470ca5a47ac9179db399c9c4f
Kirill <kirill.parhomenko@gmail.com>
2019-04-10 22:55:53 +0500

    UPD: charToIndex method

--- rsa/ViewController.swift
+++ rsa/ViewController.swift
@@ -40,7 +40,11 @@ class ViewController: NSViewController {
     @IBOutlet weak var msgLabel: NSTextField!
     
     @IBAction func encryptBtn(_ sender: NSButton) {
-        //print(arrayOfSymbol)
+        let charIndexArray = charToIndex(arrayOfSymbols: arrayOfSymbols)
+        //print(testString.count)
+        print(testString)
+        print(charIndexArray)
+        //print(decodedArrayOfSymbols.count)
     }
     @IBAction func decryptBtn(_ sender: NSButton) {
     }
@@ -48,35 +52,41 @@ class ViewController: NSViewController {
     
     //main code
     let generator = Generator()
+    
     lazy var tempP = generator.generateP()
     lazy var tempQ = generator.generateQ()
+    
     var tempEuler = EulerGenerator()
     let PQParams = PQChecker()
+    
     lazy var params = PQParams.checkPQ(p: tempP, q: tempQ)
     lazy var p = params.0
     lazy var q = params.1
+    
     lazy var euler = tempEuler.getEuler(p: p, q: q)
     lazy var paramD = generator.generateD(euler: euler)
     lazy var paramE = generator.generateE(paramD: paramD, euler: euler)
     
     
     //alphabet arrays
-    let symbolsArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "э", "ю", "я", "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Э", "Ю", "Я", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "&", "?", "-", "+", "=", "~"]
+    let alphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "э", "ю", "я", "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Э", "Ю", "Я", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "&", "?", "-", "+", "=", "~", " "]
     
     let testString = "Super secret message!"
+    lazy var arrayOfSymbols = testString.splitedBy(length: 1)
+
     
-    lazy var arrayOfSymbols: Array = testString.splitedBy(length: 1)
-    
-    var decodedArrayOfSymbols = [String]()
-    
-    func isStringContainSymbols(arrayOfSymbols: String) -> [String] {
-        let stringLength = arrayOfSymbols.count
-        for _ in 0...stringLength {
-//            if (symbolArray.contains(arrayOfSymbol[i])) {
-//                decodedArrayOfSymbol +=
-//            }
+    func charToIndex(arrayOfSymbols: [String]) -> [String] {
+        var charIndexArray = [String]()
+        for elementIndex in 0...arrayOfSymbols.count - 1 {
+            var charInArrayOfSymbols: String = arrayOfSymbols[elementIndex]
+            
+            if alphabetArray.contains(charInArrayOfSymbols) {
+                var dataToAppend: Int = alphabetArray.firstIndex(of: charInArrayOfSymbols) ?? 0
+                charIndexArray.append(String(dataToAppend))
+            }
         }
-        return decodedArrayOfSymbols
+        
+        return charIndexArray
     }
     
     
